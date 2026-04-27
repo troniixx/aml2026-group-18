c5e6306de0161f546b3b18536674bea5182f094d
Jenifer Leleany <102660673+jeniferleleanymeyer@users.noreply.github.com>
2026-04-27 14:22:15 +0200

    config-file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+DATA_DIR = "data/train"
+MODEL_PATH = "src/models/svm_model.pkl"
+
+CLASSES = [
+    'bird', 'boar', 'dog', 'dragon', 'hare', 
+    'horse', 'monkey', 'ox', 'ram', 'rat', 
+    'snake', 'tiger', 'zero'
+]
