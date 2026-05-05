5df15ee7bfbd832d335bd1aed11274a2a409b7b2
Jenifer Leleany Meyer <jen@overpriced-typewriter.local>
2026-05-05 13:48:09 +0200

    re-trained models

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
-DATA_DIR_TRAIN = "data\\train"
-DATA_DIR_TEST = "data\\test"
+DATA_DIR_TRAIN = "data/train"
+DATA_DIR_TEST = "data/test"
 MODEL_PATH1 = "src/models/svm_model1.pkl"
 MODEL_PATH2 = "src/models/svm_model2.pkl"
-NEW_DATA_DIR_TRAIN = "new_data\\train"
-NEW_DATA_DIR_TEST = "new_data\\test"
-NEW_DATA_DIR_VAL = "new_data\\valid"
+NEW_DATA_DIR_TRAIN = "new_data/train"
+NEW_DATA_DIR_TEST = "new_data/test"
+NEW_DATA_DIR_VAL = "new_data/valid"
+DATA_DIR_OWN = "data/own_data_processed"
 
 CLASSES = [
     'bird', 'boar', 'dog', 'dragon', 'hare', 
--- src/models/train_svm2.py
+++ src/models/train_svm2.py
@@ -6,14 +6,35 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.data.preprocess2 import build_dataset
-from src.config import DATA_DIR_TRAIN, DATA_DIR_TEST, MODEL_PATH2, CLASSES
+from src.data.preprocess1 import build_dataset, build_dataset_other
+from src.config import DATA_DIR_TRAIN, DATA_DIR_TEST, MODEL_PATH2, CLASSES, NEW_DATA_DIR_TRAIN, NEW_DATA_DIR_VAL, NEW_DATA_DIR_TEST, DATA_DIR_OWN
 
 def train():
     print('Building training dataset...')
     X_train, y_train, plotter_train = build_dataset(data_dir=DATA_DIR_TRAIN)
     print('Building test dataset...')
     X_test, y_test, plotter_test = build_dataset(data_dir=DATA_DIR_TEST)
+    print('Building training dataset...')
+    X_train_new, y_train_new = build_dataset_other(data_dir=NEW_DATA_DIR_TRAIN)
+    print('Building training dataset...')
+    X_train_new_val, y_train_new_val = build_dataset_other(data_dir=NEW_DATA_DIR_VAL)
+    print('Building training dataset...')
+    X_train_new_test, y_train_new_test = build_dataset_other(data_dir=NEW_DATA_DIR_TEST)
+    print('Building training dataset...')
+    X_train_own, y_train_own, plotter_train_own = build_dataset(data_dir=DATA_DIR_OWN)
+
+    X_1 = np.concatenate((X_train, X_train_new))
+    y_1 = np.concatenate((y_train, y_train_new))
+    X_2 = np.concatenate((X_1, X_train_new_val))
+    y_2 = np.concatenate((y_1, y_train_new_val))
+    X_3 = np.concatenate((X_2, X_train_new_test))
+    y_3 = np.concatenate((y_2, y_train_new_test))
+    X_4 = np.concatenate((X_3, X_test))
+    y_4 = np.concatenate((y_3, y_test))
+    X_5 = np.concatenate((X_4, X_train_own))
+    y_5 = np.concatenate((y_4, y_train_own))
+
+    X_train, X_test, y_train, y_test = train_test_split(X_5, y_5, test_size=0.25, random_state=42)
 
     # PLOT
     ################################################################
@@ -44,7 +65,7 @@ def train():
     ax.set_xticklabels(lab)
     ax.legend()
 
-    plt.show()
+    # plt.show()
     ################################################################
 
     model = svm.SVC(kernel='rbf', probability=True)
@@ -53,7 +74,7 @@ def train():
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
 
-    print(f"Accuracy: {acc:.4f}")
+    print(f"Accuracy: {acc*100:.4f}")
 
     os.makedirs(os.path.dirname(MODEL_PATH2), exist_ok=True)
     joblib.dump(model, MODEL_PATH2)
