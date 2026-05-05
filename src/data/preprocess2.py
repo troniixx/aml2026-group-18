a2bcb3acd1cc59ac6422161e59784065a6234a67
Jenifer Leleany Meyer <jen@overpriced-typewriter.local>
2026-05-05 13:48:09 +0200

    re-trained models

--- src/data/preprocess2.py
+++ src/data/preprocess2.py
@@ -3,7 +3,7 @@ import numpy as np
 from src.features.landmarks2 import extract_landmarks
 import cv2
 from tqdm import tqdm
-from src.config import NEW_CLASSES, NEW_DATA_DIR_TEST, NEW_DATA_DIR_TRAIN, NEW_DATA_DIR_VAL
+from src.config import NEW_CLASSES, CLASSES
 
 def build_dataset(data_dir):
     X, y = [], []
@@ -11,8 +11,11 @@ def build_dataset(data_dir):
 
     plotter = []
 
-    for label in os.listdir(data_dir):
-        class_dir = cwd + '\\' + data_dir + '\\' + label
+    classes = CLASSES if 'zero' in os.listdir(data_dir) else CLASSES[:-1]
+    classes = CLASSES[:-1]
+
+    for label in classes:
+        class_dir = cwd + '/' + data_dir + '/' + label
         counter = 0
 
         for img_name in tqdm(os.listdir(class_dir), desc=f"Processing '{label}'"):
@@ -28,18 +31,6 @@ def build_dataset(data_dir):
 
         print(f"processed {counter} images for class '{label}'")
         plotter.append([label, counter])
-
-    for d in [NEW_DATA_DIR_TRAIN, NEW_DATA_DIR_TEST, NEW_DATA_DIR_VAL]:
-        for pic in tqdm(os.listdir(d), desc=f"Processing additional data"):
-            if pic.endswith('.jpg'):
-                img_path = os.path.join(d, pic)
-                image = cv2.imread(img_path)
-
-            features = extract_landmarks(image)
-
-            if features is not None:
-                X.append(features)
-                y.append('snake' if pic.split('_')[0] == 'serpent' else pic.split('_')[0])
     
     X_clean, y_clean = [], []
 
@@ -49,3 +40,27 @@ def build_dataset(data_dir):
             y_clean.append(label)
 
     return np.array(X_clean), np.array(y_clean), plotter
+
+def build_dataset_other(data_dir):
+    X, y = [], []
+    cwd = os.getcwd()
+
+    for pic in tqdm(os.listdir(data_dir), desc=f"Processing additional data"):
+        if pic.endswith('.jpg'):
+            img_path = os.path.join(data_dir, pic)
+            image = cv2.imread(img_path)
+
+        features = extract_landmarks(image)
+
+        if features is not None:
+            X.append(features)
+            y.append('snake' if pic.split('_')[0] == 'serpent' else pic.split('_')[0])
+    
+    X_clean, y_clean = [], []
+
+    for x, label in zip(X, y):
+        if x is not None and len(x) > 0:
+            X_clean.append(x)
+            y_clean.append(label)
+    
+    return np.array(X_clean), np.array(y_clean)
